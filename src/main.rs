569dce9d32873ca01f68ba776c016786d4420dee
Daniel Owen van Dommelen <daniel.van.dommelen@gmail.com>
2018-02-14 16:34:57 +0100

    Playing with GET requests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate termion;
+extern crate hyper;
 
-use std::{i8};
-use std::io::{stdin, stdout, Write};
+use std::io::{self, stdin, stdout, Read, Write};
 use termion::{clear, color};
+use hyper::Client;
 
 fn main() {
     // Clear the terminal!
@@ -12,9 +13,23 @@ fn main() {
     println!("{yellow}AM Shell v0.1.0", yellow=color::Fg(color::Yellow));
     println!("Written by Daniel Owen van Dommelen\n");
 
+    // Test out a quick GET request.
+    let client = Client::new();
+    let url = "http://httpbin.org/status/418";
+    let mut response = match client.get(url).send() {
+        Ok(response) => response,
+        Err(_) => panic!("Whoops."),
+    };
+    let mut buf = String::new();
+    match response.read_to_string(&mut buf) {
+        Ok(_) => (),
+        Err(_) => panic!("I give up."),
+    };
+    println!("buf: {}", buf);
+
     // Main loop.
     loop {
-        print!(">");
+        print!("{white}>", white=color::Fg(color::White));
 
         let _       = stdout().flush();
         let mut cmd = String::new();
