ba298ac635be4b50e05427a3b1a30e8aa8e251b7
Daniel Owen van Dommelen <danielowenvandommelen@Daniels-MacBook-Pro.local>
2019-08-29 20:25:45 +0200

    Adding os commands and piped commands

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,66 @@
-extern crate termion;
-extern crate hyper;
-
-use std::io::{self, stdin, stdout, Read, Write};
-use termion::{clear, color};
-use hyper::Client;
+use std::env;
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
+use std::process::{Child, Command, Stdio};
 
 fn main() {
-    // Clear the terminal!
-    println!("{}", clear::All);
-
-    // Print my shit.
-    println!("{yellow}AM Shell v0.1.0", yellow=color::Fg(color::Yellow));
-    println!("Written by Daniel Owen van Dommelen\n");
-
-    // Test out a quick GET request.
-    let client = Client::new();
-    let url = "http://httpbin.org/status/418";
-    let mut response = match client.get(url).send() {
-        Ok(response) => response,
-        Err(_) => panic!("Whoops."),
-    };
-    let mut buf = String::new();
-    match response.read_to_string(&mut buf) {
-        Ok(_) => (),
-        Err(_) => panic!("I give up."),
-    };
-    println!("buf: {}", buf);
-
-    // Main loop.
     loop {
-        print!("{white}>", white=color::Fg(color::White));
+        print!(">");
+        stdout().flush().unwrap();
 
-        let _       = stdout().flush();
-        let mut cmd = String::new();
-        let input   = stdin().read_line(&mut cmd);
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
+
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+
+                    previous_command = None;
+                },
+                "exit" => return,
+                command => {
+                    let stdin = previous_command
+                        .map_or(Stdio::inherit(),
+                            |output: Child| Stdio::from(output.stdout.unwrap()));
+                    
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => {previous_command = Some(output);},
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        }
+                    };
+                }
+            }
+        }
+
+        if let Some(mut final_command) = previous_command {
+            final_command.wait().unwrap();
+        }
     }
 }
